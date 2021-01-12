8c9d4499a957aeb3ce13e09a282106a40e5616ae
Perry Defayette <defayetteperry@gmail.com>
2021-01-12 19:41:42 +0000

    Completed with a callback waterfall.

--- survey.js
+++ survey.js
@@ -1,2 +1,38 @@
 //profile generator survey
 
+const { RSA_X931_PADDING } = require('constants');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? Nicknames are also acceptable :)', (name) => {
+  rl.question('What\'s an activity you like doing?', (activity) => {
+    rl.question('What do you listen to while doing that?', (sound) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal?', (food) => {
+          rl.question('Which sport is your absolute favourite?', (sport) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (superPower) => {
+              console.log(`${name} loves listening to ${sound} while ${activity}, devouring ${food} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superPower}.\n`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
+
+/*
+What's your name? Nicknames are also acceptable :)
+What's an activity you like doing?
+What do you listen to while doing that?
+Which meal is your favourite (eg: dinner, brunch, etc.)
+What's your favourite thing to eat for that meal?
+Which sport is your absolute favourite?
+What is your superpower? In a few words, tell us what you are amazing at!
+*/
